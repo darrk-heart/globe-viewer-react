870cbcd826c388f1ca15d4efe8113d86b13f7fea
darrk-heart <prospaul999@gmail.com>
2024-03-10 08:51:15 +0000

    ignored the toggle theme functionality

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import moon from "../assets/icon-moon.svg";
-import sun from "../assets/icon-sun.svg";
 import "../styles/Heading.css";
+import moon from "../assets/icon-moon.svg";
 
-function Heading({ toggleTheme, isDarkMode }) {
+function Heading() {
   return (
     <div className="head">
       <div className="small-head">
         <h2>Where in the world</h2>
-        <div className="mode-toggle" onClick={toggleTheme}>
-          <img
-            className="sun-moon"
-            src={isDarkMode ? sun : moon}
-            alt={isDarkMode ? "sun icon" : "moon icon"}
-          />
-          <span>{isDarkMode ? "Light Mode" : "Dark Mode"}</span>
+        <div className="mode-toggle">
+          <img src={moon} alt="moon icon" className="sun-moon" />
+          <span>Dark Mode</span>
         </div>
       </div>
     </div>
