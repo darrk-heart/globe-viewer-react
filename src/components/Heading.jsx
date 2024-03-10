88c1ba84fd185f8cb777b217d0a91f71a4b8e163
darrk-heart <prospaul999@gmail.com>
2024-03-10 08:49:45 +0000

    still working

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import "../styles/Heading.css";
 import moon from "../assets/icon-moon.svg";
 import sun from "../assets/icon-sun.svg";
+import "../styles/Heading.css";
 
-function Heading() {
+function Heading({ toggleTheme, isDarkMode }) {
   return (
     <div className="head">
       <div className="small-head">
         <h2>Where in the world</h2>
-        <div className="mode-toggle">
-          <img src={moon} alt="moon icon" />
-          <span>Dark Mode</span>
+        <div className="mode-toggle" onClick={toggleTheme}>
+          <img
+            className="sun-moon"
+            src={isDarkMode ? sun : moon}
+            alt={isDarkMode ? "sun icon" : "moon icon"}
+          />
+          <span>{isDarkMode ? "Light Mode" : "Dark Mode"}</span>
         </div>
       </div>
     </div>
