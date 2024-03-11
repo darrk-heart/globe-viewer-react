5f435d5855ae9677b011706d873e038f224071b5
darrk-heart <prospaul999@gmail.com>
2024-03-11 17:52:12 +0000

    done with search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import searchicon from "../assets/search-button.svg";
 
 function Search() {
   return (
@@ -6,10 +7,25 @@ function Search() {
       style={{
         backgroundColor: "white",
         display: "flex",
+        width: "310px",
+        padding: "8px",
+        paddingLeft: "20px",
+        borderRadius: "4px",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
       }}
     >
-      <img />
-      <input type="text" placeholder="Search for a country" />
+      <img src={searchicon} alt="searchicon" style={{ width: "15px" }} />
+      <input
+        type="text"
+        placeholder="Search for a country"
+        style={{
+          border: "none",
+          backgroundColor: "inherit",
+          padding: "inherit",
+          outline: "none",
+          width: "80%",
+        }}
+      />
     </div>
   );
 }
