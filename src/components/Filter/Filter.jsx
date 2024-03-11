637fb912f61e90baa99e33265bb02e6661ffb240
darrk-heart <prospaul999@gmail.com>
2024-03-11 18:17:22 +0000

    on it

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Filter.css";
 
 function Filter() {
   return (
-    <div>
-      <div>Filter by Region</div>
+    <div className="filter">
+      <div className="filter-div">Filter by Region</div>
+      <div className="lists">
+        <ul>
+          <li>Africa</li>
+          <li>America</li>
+          <li>Asia</li>
+          <li>Europe</li>
+          <li>Oceania</li>
+        </ul>
+      </div>
     </div>
   );
 }
