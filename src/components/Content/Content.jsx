c53e42d3ebd306b5191ccd9ce2234117b6170358
darrk-heart <prospaul999@gmail.com>
2024-03-12 08:32:23 +0000

    working on 8 random countries

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import countriesData from "../../data/data.json";
 
 function Content() {
-  return <div style={{ backgroundColor: "#f5f5f5" }}>hello world</div>;
+  const [randomCountries, setRandomCountries] = useState([]);
+
+  const selectRandomCountries = () => {
+    const shuffledCountries = countriesData.sort(() => 0.5 - Math.random());
+    const selectedCountries = shuffledCountries.slice(0, 8);
+    setRandomCountries(selectedCountries);
+  };
+
+  useState(() => {
+    selectRandomCountries();
+  }, []);
+  // })
+  return (
+    <div>
+      <div>
+        {randomCountries.map((country) => (
+          <div key={country.name}>
+            <img
+              src={country.flag}
+              alt={`${country.name} flag`}
+              width="50"
+              height="30"
+            />
+            <p>Name: {country.name}</p>
+            <p>Population: {country.population}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Content;
