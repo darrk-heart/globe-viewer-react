989239a363c6756a8125484376db2fa465535320
darrk-heart <prospaul999@gmail.com>
2024-03-12 07:17:39 +0000

    working on that filter page becomign hard

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,9 +3,14 @@ import "./Filter.css";
 
 function Filter() {
   return (
-    <div className="filter">
-      <div className="filter-div">Filter by Region</div>
-      <div className="lists">
+    <div className="hey">
+      <div className="filter">
+        <div className="filter-div">
+          <span>Filter by Region</span>
+          <span style={{ fontWeight: "800" }}> > </span>
+        </div>
+      </div>
+      <div className="lists" style={{}}>
         <ul>
           <li>Africa</li>
           <li>America</li>
