fb0a50d24c0b468602b25c259493ea6e50f3d50e
darrk-heart <prospaul999@gmail.com>
2024-03-12 07:26:12 +0000

    using a different idea

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import "./Filter.css";
+import dropdown from "../../assets/drop-down.png";
 
 function Filter() {
   return (
-    <div className="hey">
-      <div className="filter">
-        <div className="filter-div">
-          <span>Filter by Region</span>
-          <span style={{ fontWeight: "800" }}> > </span>
-        </div>
-      </div>
-      <div className="lists" style={{}}>
-        <ul>
-          <li>Africa</li>
-          <li>America</li>
-          <li>Asia</li>
-          <li>Europe</li>
-          <li>Oceania</li>
-        </ul>
-      </div>
+    <div
+      className="filter"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: "white",
+        padding: "10px",
+        borderRadius: "4px",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <span>Filter by Region</span>
+      <img
+        src={dropdown}
+        alt="drop down icon "
+        style={{ width: "15px", height: "auto" }}
+      />
     </div>
   );
+  // {
+  //   /* <div className="lists" style={{}}>
+  //       <ul>
+  //         <li>Africa</li>
+  //         <li>America</li>
+  //         <li>Asia</li>
+  //         <li>Europe</li>
+  //         <li>Oceania</li>
+  //       </ul>
+  //     </div> */
+  // }
 }
 
 export default Filter;
