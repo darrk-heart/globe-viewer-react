20a53a441baf07111a9e745ecf30d7fd077306cc
darrk-heart <prospaul999@gmail.com>
2024-03-12 07:58:34 +0000

    heading almost done

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,31 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import dropdown from "../../assets/drop-down.png";
 
-function Filter() {
+const FilterComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div
       className="filter"
       style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "white",
-        padding: "15px",
-        borderRadius: "4px",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-        width: "170px",
+        position: "relative",
+        display: "inline-block",
       }}
     >
-      <span style={{ fontWeight: "600", fontSize: "0.9rem" }}>
-        Filter by Region
-      </span>
-      <img
-        src={dropdown}
-        alt="drop down icon "
-        style={{ width: "15px", height: "auto" }}
-      />
+      <div
+        className="filter-header"
+        onClick={toggleDropdown}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "white",
+          padding: "15px",
+          borderRadius: "4px",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+          cursor: "pointer",
+          minWidth: "170px",
+        }}
+      >
+        <span style={{ fontWeight: "600", fontSize: "0.9rem" }}>
+          Filter by Region
+        </span>
+        <img
+          src={dropdown}
+          alt="drop down icon "
+          style={{ width: "15px", height: "auto" }}
+        />
+      </div>
+      {isOpen && (
+        <div
+          className="dropdown-content"
+          style={{
+            position: "absolute",
+            top: "105%",
+            left: 0,
+            backgroundColor: "white",
+            borderRadius: "4px",
+            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            width: "100%",
+          }}
+        >
+          <ul
+            style={{
+              listStyleType: "none",
+              display: "flex",
+              flexDirection: "column",
+              lineHeight: "2",
+              fontWeight: "500",
+              fontSize: "0.9rem",
+              color: "#444444",
+              cursor: "pointer",
+            }}
+          >
+            <li>Africa</li>
+            <li>America</li>
+            <li>Asia</li>
+            <li>Europe</li>
+            <li>Oceania</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default Filter;
+export default FilterComponent;
