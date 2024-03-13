80305ac3449738562d2603810ecad1bad549cf9b
darrk-heart <prospaul999@gmail.com>
2024-03-13 16:35:19 +0000

    got to take my time with this

---
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CountryDetails() {
+  return (
+    <div>
+      {/* <h1>{country.name}</h1>
+      <p>Population: {country.population}</p> */}
+    </div>
+  );
+}
+
+export default CountryDetails;
