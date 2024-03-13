205587903c91293f7d2a50d871108b02e7baa01a
darrk-heart <prospaul999@gmail.com>
2024-03-13 03:07:14 +0000

    everything rendering well for the time being

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -7,7 +7,7 @@ function Content() {
 
   const selectRandomCountries = () => {
     const shuffledCountries = countriesData.sort(() => 0.5 - Math.random());
-    const selectedCountries = shuffledCountries.slice(0, 4);
+    const selectedCountries = shuffledCountries.slice(0, 5);
     setRandomCountries(selectedCountries);
   };
 
@@ -17,6 +17,28 @@ function Content() {
 
   return (
     <>
+      <div className="rows">
+        {randomCountries.map((country) => (
+          <div className="countries" key={country.name}>
+            <img
+              src={country.flag}
+              alt={`${country.name} flag`}
+              width="220"
+              height="150"
+            />
+            <h4>{country.name}</h4>
+            <p>
+              <span>Population:</span> {country.population}
+            </p>
+            <p>
+              <span>Region:</span> {country.region}
+            </p>
+            <p>
+              <span>Capital:</span> {country.capital}
+            </p>
+          </div>
+        ))}
+      </div>
       <div className="rows">
         {randomCountries.map((country) => (
           <div className="countries" key={country.name}>
